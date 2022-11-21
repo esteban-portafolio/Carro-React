98b96cc909d99677feb87d7e71104f9a853a0778
Esteban <e.mendoza.g93@gmail.com>
2022-11-20 21:28:26 -0300

    NavBar Section

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Content from './components/Content.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Content/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <button id="btn-carrito" type="button" data-bs-toggle="collapse" data-bs-target="#btn-items"
+          aria-expanded="false" aria-controls="Carrito">
+          <ion-icon ios="cart-outline" md="cart-sharp"></ion-icon>
+          <span id="contadorCarrito">0</span>
+        </button>
+        <a className="navbar-brand" href="index.html">
+          <img src="./src/img/cap2.png" className="img-fluid" width="80px" height="80px"/> <span id='span'>Super Tiendita</span>
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">Productos</a>
+            </li>
+            {/* <li className="nav-item">
+              <a className="nav-link" href="contacto.html">Contacto</a>
+            </li> */}
+          </ul>
+          <input value="search" className="form-control d-flex w-25 m-2" id="buscaInput" type="search"
+            placeholder="Buscar" aria-label="Search"/>
+        </div>
+
+      </div>
+    </nav>
+
+  )
+}
+
+export default Navbar
